42ed18f5aab2c24038b7afee34c8e9d552b3b50d
Karthik Dasari <karthik.d.parsuram@gmail.com>
2023-07-22 13:36:23 +0530

    Python commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const KitchenController = require("./KitchenController");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { exec } = require('child_process');
 
 const app = express();
 const port = 3000;
 
+
 app.use(bodyParser.json());
 
+// GET method to call the Python script
+app.get('/process', (req, res) => {
+  const inputData = req.query.input; // Assuming the input is passed as a query parameter named 'input'
+
+  // Call the Python script using the child_process module
+  exec(`python process_data.py ${inputData}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing the Python script: ${error.message}`);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+    
+    // Process the output received from the Python script
+    const processedData = stdout.trim();
+    
+    // Return the processed data as a JSON response
+    return res.json({ processedData });
+  });
+});
 
 app.get("/", (req, res) => {
   const controller = new KitchenController();
---
+++ src/Process_data.py
@@ -0,0 +1,6 @@
+# process_data.py
+
+def process_data(input_data):
+    # Perform some data processing here
+    # For demonstration purposes, let's return the input data in uppercase
+    return input_data.upper()
